ac2f290b14d416ca325579022af7d2c906e8828f
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-19 23:53:45 +0600

    Footer position fixed

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='bg-dark p-3'>
+        <div className='bg-dark p-3 fixed-bottom w-100'>
             <h1 className='text-center text-white'>Footer</h1>
         </div>
     )
