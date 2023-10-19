e7caadfc20cb78df62ff84a81c006ca05044b3d8
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-19 23:47:04 +0600

    Routing changes to react router link

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,41 +1,50 @@
-import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import { unSetUserToken } from '../features/authSlice'
-import { getToken, removeToken } from '../services/localStoregService'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { unSetUserToken } from '../features/authSlice';
+import { getToken, removeToken } from '../services/localStoregService';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+
 const NavBar = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const { access_token } = getToken()
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { access_token } = getToken();
 
     const handleLogout = () => {
-        dispatch(unSetUserToken({ access_token: null }))
-        removeToken()
-        navigate('/login')
-    }
+        dispatch(unSetUserToken({ access_token: null }));
+        removeToken();
+        navigate('/login');
+    };
+
     return (
-        <Navbar collapseOnSelect expand="lg" variant='dark' bg='dark'>
+        <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
             <Container>
-                <Navbar.Brand href="/">Ecom-Frontend</Navbar.Brand>
+                <Link to="/">
+                    <Navbar.Brand>Ecom-Frontend</Navbar.Brand>
+                </Link>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
+                        <Link to="/" className="nav-link">
+                            Home
+                        </Link>
+                        <Link to="/about" className="nav-link">
+                            About
+                        </Link>
                     </Nav>
                     <Nav>
-                        {access_token ?
+                        {access_token ? (
                             <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-                            :
-                            <Nav.Link href="/login">Login</Nav.Link>
-                        }
-
+                        ) : (
+                            <Link to="/login" className="nav-link">
+                                Login
+                            </Link>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
